76055118ee588724b905abdebe85e60a12e46d12
Jay <jaypranil1503@gmail.com>
2026-09-06 02:26:21 +0530

    made a basic register view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import "package:firebase_core/firebase_core.dart";
 import 'package:flutter/material.dart';
+import "package:firebase_auth/firebase_auth.dart";
+import "firebase_options.dart";
 
 void main() {
   runApp(MaterialApp(
@@ -11,8 +14,31 @@ void main() {
     ),);
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const new({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override 
+  void initState(){
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override 
+  void dispose(){
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +46,43 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title : const Text("Register"),
       ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            enableSuggestions: false,
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              hintText: "Enter your email",
+            ),
+          ),
+          TextField(
+            controller: _password,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: InputDecoration(
+              hintText: "Enter your password"
+            ),
+          ),
+          TextButton(
+            onPressed: () async {
+
+              await Firebase.initializeApp(
+                options: DefaultFirebaseOptions.currentPlatform,
+              );
+
+              final email = _email.text;
+              final password = _password.text;
+              final userCredential = FirebaseAuth.instance.createUserWithEmailAndPassword(
+                email: email, 
+                password: password
+                );
+            }, 
+            child: const Text("Register")),
+        ],
+      )
     );
   }
 }
