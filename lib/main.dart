40d69d283b4b6b83440ac0b6ba712a63099d64f3
Jay <jaypranil1503@gmail.com>
2026-09-14 17:45:00 +0530

    Added email verifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import "package:firebase_auth/firebase_auth.dart";
 import "package:firebase_core/firebase_core.dart";
 import 'package:flutter/material.dart';
 import "firebase_options.dart";
@@ -31,12 +32,14 @@ class HomePage extends StatelessWidget {
           switch(asyncSnapshot.connectionState)
           {
             case ConnectionState.done :
-
-              if(asyncSnapshot.hasError){
-                return Center(child : Text("Error: ${asyncSnapshot.error}"),);
+              final user = FirebaseAuth.instance.currentUser ;
+              if(user?.emailVerified ?? false){
+                print("User is verified");
+              }else{
+                print("User is not verified");
+                return const VerifyEmailView();
               }
               return const Text("Done");
-
           default :
             return const Text ("Loading");
 
@@ -46,5 +49,28 @@ class HomePage extends StatelessWidget {
           
       )
     );
+  }
+}
+
+class VerifyEmailView extends StatefulWidget {
+  const VerifyEmailView({super.key});
+
+  @override
+  State<VerifyEmailView> createState() => _VerifyEmailViewState();
+}
+
+class _VerifyEmailViewState extends State<VerifyEmailView> {
+  @override
+  Widget build(BuildContext context) {
+    return  Column(children : [
+          Text("Please verify your email address: "),
+          TextButton(
+            onPressed : () async {
+              final user = FirebaseAuth.instance.currentUser;
+              await user?.sendEmailVerification();
+            },
+            child : const Text("Send email verification")
+          )
+        ]);
   }
 }
