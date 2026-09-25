bc04a86ed50b721e76fa224efe06c0745b7723fd
Jay <jaypranil1503@gmail.com>
2026-09-25 23:46:52 +0530

    Cleaned the code a bit

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import "package:firebase_core/firebase_core.dart";
 import 'package:flutter/material.dart';
 import "package:my_notes/constants/routes.dart";
 import "package:my_notes/views/login_view.dart";
+import "package:my_notes/views/notes_view.dart";
 import "package:my_notes/views/register_view.dart";
 import "package:my_notes/views/verify_email_view.dart";
 import "firebase_options.dart";
@@ -61,75 +62,5 @@ class HomePage extends StatelessWidget {
   }
 }
 
-enum MenuAction {
-  logout,
-}
 
-class NotesView extends StatefulWidget {
-  const NotesView({super.key});
 
-  @override
-  State<NotesView> createState() => _NotesViewState();
-}
-
-class _NotesViewState extends State<NotesView> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar : AppBar(
-        title : const Text("Main UI"),
-        actions : [
-          PopupMenuButton<MenuAction>(
-            onSelected : (value) async {
-              switch(value) {
-                
-                case MenuAction.logout:
-                  final shouldLogout = await showLogOutDialog(context);
-                  if (shouldLogout) {
-                    FirebaseAuth.instance.signOut();
-                    Navigator.of(context).pushNamedAndRemoveUntil(
-                      loginRoute, 
-                      (_) => false,
-                    );
-                  }
-              }
-            },
-            itemBuilder : (context) {
-              return const [
-                PopupMenuItem<MenuAction>(
-                value : MenuAction.logout,
-                child : Text("Logout"),
-                ),
-              ];
-          },)
-        ],
-      )
-    );
-  }
-}
-
-Future<bool> showLogOutDialog(BuildContext context){
-   return showDialog<bool>(
-    context : context,
-    builder : (context) {
-      return AlertDialog(
-        title : const Text("Sign out"),
-        content : const Text("Are you sure you want to sign out?"),
-      actions : [
-        TextButton (
-          onPressed: () {
-            Navigator.of(context).pop(true);
-          },
-          child : const Text("Logout")
-        ),
-        TextButton (
-          onPressed: () {
-            Navigator.of(context).pop(false);
-          },
-          child : const Text("Cancel")
-        ),
-      ]
-      );
-    }
-  ).then((value) => value ?? false);
-}
--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,10 +1,13 @@
 import "package:my_notes/services/auth/auth_provider.dart";
 import "package:my_notes/services/auth/auth_user.dart";
+import "package:my_notes/services/auth/firebase_auth_provider.dart";
 
 class AuthService implements AuthProvider {
 
   final AuthProvider provider;
   const AuthService(this.provider);
+
+  factory AuthService.firebase() => AuthService(FirebaseAuthProvider());
 
   @override
   Future<AuthUser> createUser({
